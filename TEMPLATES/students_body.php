6137a679b462fbbca95c0382818c3d56da76edb1
brettdinelli <brettdinelli@172.28.0.81>
2017-11-30 15:07:56 -0400

    finalized the student report table

--- TEMPLATES/students_body.php
+++ TEMPLATES/students_body.php
@@ -27,23 +27,44 @@
                 // 2.  connect to the database
                 require MYSQL;
         
-                $q = "select * from grads";
+                $sql = "select count(*) from grads";
+                $stmt = $dbc->query($sql);
+                $cnt = $stmt->fetchColumn();
+                
+                // echo "<h2>Total Grads: $cnt</h2>";
+                
+                // $q = "SELECT fname, lname, gradyear, linkedin FROM grads;";
+                $q = "select concat(fname, ' ', lname) as Name, gradyear as 'Grad Year', company_name as 'Company', title_name as Title
+                      from grads join employment on grads.grad_id = employment.grad_id
+                      join companies on employment.company_id = companies.company_id
+                      join titles on employment.title_id = titles.title_id
+                      order by gradyear desc, name;";
+                
                 $stmt = $dbc->query($q);
-                // var_dump($stmt);
                 
-                $grad_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $grad_query = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 
-                //var_dump($grad_list);
-                
+                // var_dump($grad_query);
+
                 echo "<table class='table table-bordered table-striped'>
-                    <thead class='thead-dark'>
+                    <thead class='thead-inverse'>
                         <tr>
                             <th>Name</th>
                             <th>Grad Year</th>
-                            <th>LinkedIn</th>
+                            <th>Company</th>
+                            <th>Title</th>
                         </tr>
                     </thead>
                     <tbody>";
+                
+                foreach($grad_query as $row) {
+                    echo "<tr>
+                            <td>{$row['Name']}</td>
+                            <td>{$row['Grad Year']}</td>
+                            <td>{$row['Company']}</td>
+                            <td>{$row['Title']}</td>
+                          </tr>";
+                }
         
         echo "</tbody></table>";
         
