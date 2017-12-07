fc4f846d6de72db79a5353a6f1d84aebcc3e6444
danie <danie@172.28.0.82>
2017-12-07 15:12:01 -0400

    admin & user page

---
+++ TEMPLATES/user_body.php
@@ -0,0 +1,19 @@
+  <div class="content-wrapper">
+    <div class="container-fluid">
+        
+     <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Home</li>
+       </ol>
+    </div>
+      
+  
+      
+      
+      
+    <div class="container-fluid">
+        <div class="col-lg-12"></div>
+    </div>
+  </div>
