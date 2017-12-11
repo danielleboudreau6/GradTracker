c29f0968f62f10d84f5fd96855f181282d2c9ffa
brettdinelli <brettdinelli@172.28.0.81>
2017-12-11 09:01:04 -0400

    working on addCompany

---
+++ TEMPLATES/addCompany_body.php
@@ -0,0 +1,15 @@
+  <div class="content-wrapper">
+    <div class="container-fluid">
+        
+       <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">
+          Students
+        </li>
+        <!--<li class="breadcrumb-item active">Home</li>-->
+       </ol>
+        
+       
+    </div>
---
+++ addCompany.php
@@ -0,0 +1,4 @@
+<?php
+include 'includes/header_section.php';
+include 'templates/addCompany_body.php';
+include 'includes/footer_section.php';
