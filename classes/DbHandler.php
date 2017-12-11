a18b99d75fb4dfe425c6042a7e34c44f40fcf2ef
danie <danie@172.28.0.82>
2017-12-11 11:04:15 -0400

    modified dbhandler

--- classes/DbHandler.php
+++ classes/DbHandler.php
@@ -52,6 +52,9 @@ class DbHandler{
      * getCategoryList() function
      * Get a list of categories for creating menu
      */
+    
+    // ---------------------------------------------- DATABASE QUERIES ----------------------------------------------
+    
     public function getStudents(){
         $sql = "select concat(fname, ' ', lname) as Name, gradyear as 'Grad Year', company_name as 'Company', title_name as Title
                       from grads join employment on grads.grad_id = employment.grad_id
@@ -102,64 +105,116 @@ class DbHandler{
         
     }//End of getPopularList
     
-    public function getArticle($id){
-        try{
-            //Prepare our sql query with $id param coming from 
-            //outside environment
-            $stmt=$this->conn->prepare("SELECT title,description,content
-                                        FROM pages 
-                                        WHERE id=:id");
-            //Bind our parameter
-            $stmt->bindValue(':id',$id,PDO::PARAM_INT);
-            
-            //Execute the query
-            $stmt->execute();
-            
-            //Fetch the data as an associative array
-            $page = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            
-            //Return array of data items
-            $data = array(
-                'error' =>false,
-                'items'=>$page
-            );
-            
-        } catch (PDOException $ex) {
-                $data = array('error'=>true,
-                              'message'=>$ex->getMessage()
-                             );
-        }//end of try catch
-        
-        //Return final data array
-        return $data;
-        
-    }//end of getArticle
+    // ---------------------------------------------- REGISTER USER ----------------------------------------------
     
-    
-     public function getArticles(){
-  
-            //build our sql query
-            $sql = "SELECT id, title, description FROM pages ORDER BY title";
-
-            try{
-                $stmt=$this->conn->query($sql);
-                $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                //Return array of data items
+        public function createUser($email, $password, $first_name, $last_name) {
+        //First check if user already exists in table
+        if (!$this->isUserExists($email)) {
+            //User does not exist - continue
+            //Generate password hash'
+            $password_hash = PassHash::hash($password);
+            //Generate random activation code
+            $active = md5(uniqid(rand(), true));
+            //Insert user in database using prepared statement
+            //Note:  set date_expires to yesterday (until they activate account)
+            $stmt = $this->conn->prepare("INSERT INTO users(email,pass,first_name,last_name,date_expires,active)
+                                          VALUES(:email, :pass, :fname, :lname, SUBDATE(NOW(), INTERVAL 1 DAY), :active)");
+            //Bind parameters
+            $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+            $stmt->bindValue(':pass', $password_hash, PDO::PARAM_STR);
+            $stmt->bindValue(':fname', $first_name, PDO::PARAM_STR);
+            $stmt->bindValue(':lname', $last_name, PDO::PARAM_STR);
+            $stmt->bindValue(':active', $active, PDO::PARAM_STR);
+            //Execute statement
+            $result = $stmt->execute();
+            //Prepare array for result
+            if ($result) {
+                // User successfully inserted
                 $data = array(
-                    'error' =>false,
-                    'items'=>$articles
-                );               
-                
-            } catch (PDOException $ex) {
-                $data = array('error'=>true,
-                              'message'=>$ex->getMessage()
-                             );
-            }//end of try catch
-        
-        //Return final data array
+                    'error' => false,
+                    'message' => 'USER_CREATE_SUCCESS',
+                    'active' => $active
+                );
+            } else {
+                // Failed to create user
+                $data = array(
+                    'error' => true,
+                    'message' => 'USER_CREATE_FAIL',
+                );
+            }
+        } else {
+            //User already exists - return error and message
+            $data = array('error' => true,
+                'message' => 'USER_ALREADY_EXISTS'
+            );
+        }
+        //Return one final data array
         return $data;
-        
-    }//end of getArticles
+    }
+//End of createUser
+}
+    
+    
+    
+    
+//    public function getArticle($id){
+//        try{
+//            //Prepare our sql query with $id param coming from 
+//            //outside environment
+//            $stmt=$this->conn->prepare("SELECT title,description,content
+//                                        FROM pages 
+//                                        WHERE id=:id");
+//            //Bind our parameter
+//            $stmt->bindValue(':id',$id,PDO::PARAM_INT);
+//            
+//            //Execute the query
+//            $stmt->execute();
+//            
+//            //Fetch the data as an associative array
+//            $page = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//            
+//            //Return array of data items
+//            $data = array(
+//                'error' =>false,
+//                'items'=>$page
+//            );
+//            
+//        } catch (PDOException $ex) {
+//                $data = array('error'=>true,
+//                              'message'=>$ex->getMessage()
+//                             );
+//        }//end of try catch
+//        
+//        //Return final data array
+//        return $data;
+//        
+//    }//end of getArticle
+//    
+//    
+//     public function getArticles(){
+//  
+//            //build our sql query
+//            $sql = "SELECT id, title, description FROM pages ORDER BY title";
+//
+//            try{
+//                $stmt=$this->conn->query($sql);
+//                $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//                //Return array of data items
+//                $data = array(
+//                    'error' =>false,
+//                    'items'=>$articles
+//                );               
+//                
+//            } catch (PDOException $ex) {
+//                $data = array('error'=>true,
+//                              'message'=>$ex->getMessage()
+//                             );
+//            }//end of try catch
+//        
+//        //Return final data array
+//        return $data;
+//        
+//    }//end of getArticles
     
      
 }//End of Class
