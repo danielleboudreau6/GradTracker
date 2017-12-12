978ac35a182c760642ba5bd1ab4fe2e0d71e145b
brettdinelli <brettdinelli@172.28.0.81>
2017-12-12 14:49:30 -0400

    got the addCompany method to work

--- TEMPLATES/addCompany_body.php
+++ TEMPLATES/addCompany_body.php
@@ -12,52 +12,39 @@
        </ol>
         
     <?php
-        // 1.  check if the form was submitted
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
-            var_dump($_POST);
-
-            //die;
-
-            $category = trim(filter_var($_POST['company_name'], FILTER_SANITIZE_STRING));
-
-            // test if the user entered something
-            if(!empty($category)) {
-                // 1.  get the configuration file (holds the connection info)
-                require './includes/config.php';
-
-                // 2.  connect to the database
-                require MYSQL;
-
-                // build the insert statement using prepared statements
-                $stmt = $dbc->prepare("INSERT INTO companies(company_name) VALUES (:company_name)");
-
-                // bind the named parameter :category to user input value
-                $stmt->bindValue(':company_name', $category, PDO::PARAM_STR);
-
-                try {
-                    // try to execute the query
-                    $stmt->execute();
-                    echo "<div class='alert alert-success' role='alert'>
-                        The company <strong>$company_name</strong> has been inserted.
-                    </div>";
-                } catch (Exception $ex) {
-                    $code = $ex->getCode();
-                    $message = 'Unknown system error';
-
-                    if($code == 23000) {
-                        $message = 'You may not insert a duplicate company';
-                    }
-                    // if an error occurs, it will be trapped here
-                    echo "<div class='alert alert-danger' role='alert'>
-                        The company <strong>$company_name</strong> was not inserted due to a system error.<br>"
-                            . $message 
-                            . "<p><a href=''>Please try again.</a></p>
-                    </div>";
-                }
-            } 
+    
+        if(isset($_SESSION['member_id'])){
+            //we found a user who is logged in - store their id in variable
+            $member_id = $_SESSION['member_id'];
+            
+        }else{
+            $member_id = null;
+        }
+        //var_dump($member_id);//null when not logged in
+        if($_POST) {
+            
+            $company_name = $_POST['company_name'];
+            $data = $dbh->addCompany($company_name);
+            
+            
+            //var_dump($data);
+            
+            if($data['error']==false) {
+                // it worked
+                // put in a success box of some kind
+                echo $data['message'];
+                 
+            }
+            else {
+                // didn't work
+                // alert box of some kind
+                echo $data['message'];
+            }
+            
+        }
+            
         
-    } else {
-
+        
         ?>
 
         <form class="form-inline" method="post" action="addCompany.php">
@@ -70,8 +57,6 @@
             <button type="submit" class="btn btn-primary">Add Company</button>
         </form>
 
-        <?php
-         }
-        ?>
+        
 
     </div>
